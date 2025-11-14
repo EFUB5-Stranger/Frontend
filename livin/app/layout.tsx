31b87daa11c366dcb4dd6d78e5d93ca666ca46d6
seooyuun <seoyuni2002@gmail.com>
2025-11-14 15:51:06 +0900

    🎁Modify: root layout SSR CSR 분리

--- livin/app/layout.tsx
+++ livin/app/layout.tsx
@@ -1,22 +1,15 @@
-'use client';
-
 import React from 'react';
-import GlobalStyle from './styles/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
+import Providers from './providers';
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang='ko'>
       <body>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          {children}
-        </ThemeProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );

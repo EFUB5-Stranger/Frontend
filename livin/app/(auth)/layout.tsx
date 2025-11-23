1ae136352c0270a7aa4ff4c174eae5f6d48e4a74
seooyuun <seoyuni2002@gmail.com>
2025-11-24 01:01:20 +0900

    Fix: RootLayout 수정

--- livin/app/(auth)/layout.tsx
+++ livin/app/(auth)/layout.tsx
@@ -4,13 +4,6 @@ import React from 'react';
 import GlobalStyle from '../styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
-import { Open_Sans } from 'next/font/google';
-import { Noto_Sans_KR } from 'next/font/google';
-
-const opensans = Open_Sans({ subsets: ['latin', 'latin-ext'] });
-const notosansKR = Noto_Sans_KR({ subsets: ['latin'] });
-
-export const fonts = `${opensans.className} ${notosansKR.className}`;
 
 export default function AuthLayout({
   children,
@@ -18,13 +11,11 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
-      <body className={fonts}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </>
   );
 }

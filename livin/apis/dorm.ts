9710a9f7a68164126421d3397080a45eabbfd370
Kim Yunji <kimyunji@i.ewha.ac.kr>
2025-11-28 17:28:44 +0900

    feat: 리뷰 댓글 조회/작성/삭제 api 연결

--- livin/apis/dorm.ts
+++ livin/apis/dorm.ts
@@ -56,19 +56,4 @@ export const uploadReviewImageApi = async (imageFile: File) => {
   return res.data; // imageUrl 반환
 };
 
-// 댓글 작성
-export const createCommentApi = async (review_id: string | number, data: {
-  content: string;
-  anonymous: boolean;
-}) => {
-  const res = await axiosInstance.post(`/review/${review_id}/comment`, data);
-  return res.data;
-};
-
-// 댓글 삭제
-export const deleteCommentApi = async (comment_id: string | number) => {
-  const res = await axiosInstance.delete(`/comment/${comment_id}`);
-  return res.status;
-};
-
 

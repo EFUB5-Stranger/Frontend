3aeb351d527319593f2ab93ff9f2d89f416f4e3f
seooyuun <seoyuni2002@gmail.com>
2025-11-30 23:54:30 +0900

    feat: favicon 추가

--- livin/app/layout.tsx
+++ livin/app/layout.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import Providers from './providers';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'livin',
+  description: 'livin web',
+  icons: {
+    icon: '/favicon.png',
+  },
+};
 
 export default function RootLayout({
   children,
